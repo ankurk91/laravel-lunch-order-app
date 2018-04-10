e2c9e82384eb1115f7f6cdf75d4a31c76d2cc5a4
ankurk91 <pro.ankurk1@gmail.com>
2018-04-10 13:19:39 +0530

    Remove local-time package

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -2,7 +2,3 @@
 window.$ = window.jQuery = require('jquery/dist/jquery.slim');
 require('popper.js');
 require('bootstrap');
-
-// https://github.com/basecamp/local_time
-import LocalTime from "local-time"
-LocalTime.start();
