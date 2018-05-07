44413b9d3d1725549630c6f9f93238f963beb7c2
ankurk91 <pro.ankurk1@gmail.com>
2018-05-07 11:21:17 +0530

    Speed up

--- resources/views/_layouts/partials/styles.blade.php
+++ resources/views/_layouts/partials/styles.blade.php
@@ -1,7 +1,8 @@
 <!-- Header styles -->
 @stack('before_vendor_styles')
-<link rel="dns-prefetch" href="//fonts.googleapis.com">
 <link href="{{ mix('css/vendor.css') }}" rel="stylesheet" media="all">
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,700,700i" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="dns-prefetch" href="//fonts.googleapis.com">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,700,700i" rel="stylesheet" crossorigin>
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" media="all">
 @stack('after_app_styles')
