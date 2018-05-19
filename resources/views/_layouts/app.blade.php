647cc0cb4ca666ae7d559875557c1c4b50e22974
ankurk91 <pro.ankurk1@gmail.com>
2018-05-19 09:39:22 +0530

    Fix referrer-policy header

--- resources/views/_layouts/app.blade.php
+++ resources/views/_layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta content="origin-when-cross-origin" name="referrer">
+  <meta content="strict-origin-when-cross-origin" name="referrer">
 
   <title> @yield('pageTitle', 'Home') | {{config('app.name')}}</title>
 
