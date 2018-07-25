8f65a82cc210aad051c68795ad0d015701dd1659
ankurk91 <pro.ankurk1@gmail.com>
2018-07-25 14:48:32 +0530

    Update bootstrap package

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -6,8 +6,7 @@ import Popper from 'expose-loader?Popper!popper.js';
 window.Popper = Popper;
 
 // Selective bootstrap.js build
-// https://github.com/twbs/bootstrap/issues/20709
-import 'expose-loader?Util!exports-loader?Util!bootstrap/js/dist/util';
+import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/collapse';
